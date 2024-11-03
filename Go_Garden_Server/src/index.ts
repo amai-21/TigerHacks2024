3030190c46975303bf8c1b4588ed975f9e0273c5
CCG226 <cown877@gmail.com>
2024-11-03 09:55:45 -0600

    last

--- Go_Garden_Server/src/index.ts
+++ Go_Garden_Server/src/index.ts
@@ -6,39 +6,16 @@ import plantRoutes from "./Routes/PlantRoutes";
 
 dotenv.config();
 const app = express();
-const helmet = require('helmet');
+
 const PORT = 5000;
 
 app.use(cors());
 
 app.use(express.json());
 
-
 // Routes
 app.use("/api", plantRoutes);
 
-
-app.use(
-  helmet({
-    contentSecurityPolicy: {
-      directives: {
-        defaultSrc: ["'self'"],
-        scriptSrc: ["'self'", "https://vercel.live"],
-        connectSrc: ["'self'", "https://vercel.live"],
-        imgSrc: ["'self'", "data:"],
-        styleSrc: ["'self'", "'unsafe-inline'"],
-      },
-    },
-  })
-);
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-
-
-
-
-
-
-
-
